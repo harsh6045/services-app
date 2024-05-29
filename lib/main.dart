06d211fc692a4d60915e430fe4b4e6780be69366
Harsh Langaliya <langaliyaharsh2003@gmail.com>
2024-05-29 11:14:03 +0530

    responsive screen commit

--- lib/main.dart
+++ lib/main.dart
@@ -1028,7 +1028,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: Column(
                         children: [
                           Padding(
-                            padding: const EdgeInsets.all(8.0),
+                            padding: const EdgeInsets.all(5.0),
                             child: Image.asset("assets/Group 1000005951(1).png",fit: BoxFit.contain,width: 130,),
                           ),
                           Row(
